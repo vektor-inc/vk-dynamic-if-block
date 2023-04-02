c02e48467ccf7262243e12a1c9ab2de6c0c0c78a
kurudrive <kurudrive@gmail.com>
2023-04-02 12:53:20 +0900

    Switch display element by post type

--- src/index.php
+++ src/index.php
@@ -1,24 +1,43 @@
 <?php
+/**
+ * Dynamic If Block
+ *
+ * @package vektor-inc/vk-dynamic-if-block
+ */
+
+use VektorInc\VK_Helpers\VkHelpers;
+
 function vk_dynamic_if_block_render( $attributes, $content ) {
-	$display_condition = isset( $attributes['pageType'] ) ? $attributes['pageType'] : 'none';
+	$page_type = isset( $attributes['pageType'] ) ? $attributes['pageType'] : 'none';
+	$post_type = isset( $attributes['selectedPostType'] ) ? $attributes['selectedPostType'] : 'none';
+
+	$return = '';
 
 	if (
-		is_front_page() && 'is_front_page' === $display_condition ||
-		is_single() && 'is_single' === $display_condition ||
-		is_page() && 'is_page' === $display_condition ||
-		is_singular() && 'is_singular' === $display_condition ||
-		is_home() && ! is_front_page() && 'is_home' === $display_condition ||
-		is_archive() && 'is_archive' === $display_condition ||
-		is_search() && 'is_search' === $display_condition ||
-		is_404() && 'is_404' === $display_condition ||
-		'none' === $display_condition
+		is_front_page() && 'is_front_page' === $page_type ||
+		is_single() && 'is_single' === $page_type ||
+		is_page() && 'is_page' === $page_type ||
+		is_singular() && 'is_singular' === $page_type ||
+		is_home() && ! is_front_page() && 'is_home' === $page_type ||
+		is_archive() && 'is_archive' === $page_type ||
+		is_search() && 'is_search' === $page_type ||
+		is_404() && 'is_404' === $page_type ||
+		'none' === $page_type
 	) {
-		return $content;
-	} else {
-		return '';
+		$return         = $content;
+		$post_type_info = VkHelpers::get_post_type_info();
+		$post_type_slug = $post_type_info['slug'];
+
+		if ( 'none' === $post_type ) {
+			$return = $content;
+		} elseif ( $post_type_slug === $post_type ) {
+			$return = $content;
+		} else {
+			$return = '';
+		}
 	}
 
-	return '';
+	return $return;
 }
 
 function vk_dynamic_if_block_register_dynamic() {
