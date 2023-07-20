82dfa2c29d0fb5bd7d85f2ddbebda12b4f7a3d11
doshimaf <daikougun0923@gmail.com>
2023-07-20 11:02:16 +0900

    処理追加

--- src/index.php
+++ src/index.php
@@ -96,6 +96,26 @@ function vk_dynamic_if_block_render( $attributes, $content ) {
 				} else {
 					$display_by_custom_field = false;
 				}
+			} elseif ( 'compareMoreLess' === $attributes['customFieldRule'] ) {
+				if ( !empty($attributes['moreThanValue']) && empty($attributes['lessThanValue']) ){
+					if ( $get_value > $attributes['moreThanValue'] ) {
+						$display_by_custom_field = true;
+					} else {
+						$display_by_custom_field = false;
+					}
+				} elseif ( !empty($attributes['lessThanValue']) && empty($attributes['moreThanValue']) ){
+					if ( $get_value < $attributes['lessThanValue'] ) {
+						$display_by_custom_field = true;
+					} else {
+						$display_by_custom_field = false;
+					}
+				} elseif ( !empty($attributes['moreThanValue']) && !empty($attributes['lessThanValue']) ){
+					if ( $get_value > $attributes['moreThanValue'] && $get_value < $attributes['lessThanValue'] ) {
+						$display_by_custom_field = true;
+					} else {
+						$display_by_custom_field = false;
+					}
+				}
 			}
 		}
 	}
