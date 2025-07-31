d576b76fd5ffbc900109dbedaa81494204dc6bd8
mtdkei <mtdkei@gmail.com>
2025-07-31 16:14:37 +0900

    Fix PHPCS errors and improve code quality

--- vk-dynamic-if-block.php
+++ vk-dynamic-if-block.php
@@ -12,81 +12,84 @@
  * @package VK Dynamic If Block
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 /**
  * Composer Autoload
  */
-$autoload_path = plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+$autoload_path = plugin_dir_path(__FILE__) . 'vendor/autoload.php';
 // Deploy failure countermeasure for Vendor directory.
-if ( file_exists( $autoload_path ) ) {
-	require_once $autoload_path;
+if (file_exists($autoload_path) ) {
+    include_once $autoload_path;
 }
 
-function vk_dynamic_if_block_enqueue_scripts() {
+function vk_dynamic_if_block_enqueue_scripts()
+{
 
-	$script_dependencies = include plugin_dir_path( __FILE__ ) . '/build/index.asset.php';
+    $script_dependencies = include plugin_dir_path(__FILE__) . '/build/index.asset.php';
 
-	// WordPress 6.5 以下の対策
-	if ( ! wp_script_is( 'react-jsx-runtime', 'registered' ) ) {
-		wp_enqueue_script(
-			'react-jsx-runtime',
-			plugins_url( 'build/react-jsx-runtime.js', __FILE__ ),
-			array( 'react' ),
-			'18.3.1',
-			true
-		);
-	}
+    // WordPress 6.5 以下の対策
+    if (! wp_script_is('react-jsx-runtime', 'registered') ) {
+        wp_enqueue_script(
+            'react-jsx-runtime',
+            plugins_url('build/react-jsx-runtime.js', __FILE__),
+            array( 'react' ),
+            '18.3.1',
+            true
+        );
+    }
 
-	$handle = 'vk-dynamic-if-block';
-	wp_enqueue_script(
-		$handle,
-		plugins_url( 'build/index.js', __FILE__ ),
-		$script_dependencies['dependencies'],
-		filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' )
-	);
+    $handle = 'vk-dynamic-if-block';
+    wp_enqueue_script(
+        $handle,
+        plugins_url('build/index.js', __FILE__),
+        $script_dependencies['dependencies'],
+        filemtime(plugin_dir_path(__FILE__) . 'build/index.js')
+    );
 
-	wp_enqueue_style(
-		'vk-dynamic-if-block-editor',
-		plugins_url( 'build/editor.css', __FILE__ ),
-		array(),
-		filemtime( plugin_dir_path( __FILE__ ) . 'build/editor.css' )
-	);
+    wp_enqueue_style(
+        'vk-dynamic-if-block-editor',
+        plugins_url('build/editor.css', __FILE__),
+        array(),
+        filemtime(plugin_dir_path(__FILE__) . 'build/editor.css')
+    );
 }
 
-add_action( 'enqueue_block_editor_assets', 'vk_dynamic_if_block_enqueue_scripts' );
+add_action('enqueue_block_editor_assets', 'vk_dynamic_if_block_enqueue_scripts');
 
-require_once plugin_dir_path( __FILE__ ) . 'build/index.php';
+require_once plugin_dir_path(__FILE__) . 'build/index.php';
 
-if ( ! function_exists( 'vk_dynamic_if_block_set_script_translations' ) ) {
-	/**
-	 * Set text domain for translations.
-	 */
-	function vk_dynamic_if_block_set_script_translations() {
-		if ( function_exists( 'wp_set_script_translations' ) ) {
-			wp_set_script_translations( 'vk-dynamic-if-block', 'vk-dynamic-if-block' );
-		}
-	}
-	add_action( 'enqueue_block_editor_assets', 'vk_dynamic_if_block_set_script_translations' );
+if (! function_exists('vk_dynamic_if_block_set_script_translations') ) {
+    /**
+     * Set text domain for translations.
+     */
+    function vk_dynamic_if_block_set_script_translations()
+    {
+        if (function_exists('wp_set_script_translations') ) {
+            wp_set_script_translations('vk-dynamic-if-block', 'vk-dynamic-if-block');
+        }
+    }
+    add_action('enqueue_block_editor_assets', 'vk_dynamic_if_block_set_script_translations');
 }
 
-if ( ! function_exists( 'vk_blocks_set_wp_version' ) ) {
-	/**
-	 * VK Blocks Set WP Version
-	 */
-	function vk_blocks_set_wp_version() {
-		global $wp_version;
+if (! function_exists('vk_blocks_set_wp_version') ) {
+    /**
+     * VK Blocks Set WP Version
+     */
+    function vk_blocks_set_wp_version()
+    {
+        global $wp_version;
 
-		// RC版の場合ハイフンを削除.
-		if ( strpos( $wp_version, '-' ) !== false ) {
-			$_wp_version = strstr( $wp_version, '-', true );
-		} else {
-			$_wp_version = $wp_version;
-		}
+        // RC版の場合ハイフンを削除.
+        if (strpos($wp_version, '-') !== false ) {
+            $_wp_version = strstr($wp_version, '-', true);
+        } else {
+            $_wp_version = $wp_version;
+        }
 
-		echo '<script>',
-			'var wpVersion = "' . esc_attr( $_wp_version ) . '";',
-		'</script>';
-	}
-	add_action( 'admin_head', 'vk_blocks_set_wp_version' );
+        echo '<script>',
+        'var wpVersion = "' . esc_attr($_wp_version) . '";',
+        '</script>';
+    }
+    add_action('admin_head', 'vk_blocks_set_wp_version');
 }
