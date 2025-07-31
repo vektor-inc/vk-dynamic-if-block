1fb898345d2c52204eb4d80cee0732d98b4e09c4
kurudrive <kurudrive@gmail.com>
2025-07-31 11:57:31 +0900

    アラートを英語に変更

--- admin-notice.php
+++ admin-notice.php
@@ -8,13 +8,13 @@ add_action( 'admin_notices', function () {
 	}
 	?>
 	<div class="notice notice-warning is-dismissible" style="padding:1.2rem;">
-		<h3 style="margin-top:0;"><?php esc_html_e( 'VK Dynamic If Block から重要なお知らせ', 'vk-dynamic-if-block' ); ?></h2>
+		<h3 style="margin-top:0;"><?php esc_html_e( 'Important Notice from VK Dynamic If Block', 'vk-dynamic-if-block' ); ?></h3>
 		<p>
-			<?php echo esc_html__( 'VK Dynamic If Block Version 1.0 から、複数の条件を組み合わせられるように条件分岐の指定方法が変更されます。', 'vk-dynamic-if-block' ); ?><br>
-			<?php echo esc_html__( 'しかしながら、プラグインをアップデートしただけではブロックエディタの構造上、条件分岐が効かなくなります。', 'vk-dynamic-if-block' ); ?><br>
-			<?php echo esc_html__( 'プラグインをアップデートした上で VK Dynamic If Block を配置している編集画面を一度開いて保存してください。', 'vk-dynamic-if-block' ); ?><br>
-			<?php echo esc_html__( 'つきましては、新バージョンの一般リリースに先立ち、以下よりダウンロードできるようにしてあります。', 'vk-dynamic-if-block' ); ?><br>
-			<?php echo esc_html__( '手動にて「プラグイン > 新規追加 > ファイルのアップロード」からアップロード・有効化した上で、VK Dynamic If Block を利用しているテンプレートファイルなどを開いて保存してください。', 'vk-dynamic-if-block' ); ?>
+			<?php echo esc_html__( 'From VK Dynamic If Block Version 1.0, the way to specify conditional branching will change to allow combining multiple conditions.', 'vk-dynamic-if-block' ); ?><br>
+			<?php echo esc_html__( 'However, due to the structure of the block editor, simply updating the plugin will cause conditional branching to stop working.', 'vk-dynamic-if-block' ); ?><br>
+			<?php echo esc_html__( 'After updating the plugin, please open and save the edit screen where VK Dynamic If Block is placed.', 'vk-dynamic-if-block' ); ?><br>
+			<?php echo esc_html__( 'Ahead of the official release of the new version, it is available for download below.', 'vk-dynamic-if-block' ); ?><br>
+			<?php echo esc_html__( 'Please manually upload and activate it from "Plugins > Add New > Upload Plugin", then open the template that uses the VK Dynamic If Block in the block editor and save it.', 'vk-dynamic-if-block' ); ?>
 		</p>
 		<a href="https://downloads.wordpress.org/plugin/vk-dynamic-if-block.1.0.0.zip" target="_blank" class="button button-primary"><?php echo esc_html__( 'Download', 'vk-dynamic-if-block' ); ?></a>
 	</div>
